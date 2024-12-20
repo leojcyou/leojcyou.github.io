e8c4f9f95b1a41c8d2e8b71ae0871f7582eaddb5
Leo <108024684+leojcyou@users.noreply.github.com>
2024-12-20 00:08:51 -0500

    features

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const NotFoundPage: React.FC = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/");
+    }, 100000);
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
-    <div>
-      <h2>NOT FOUND</h2>
+    <div className="container mx-auto">
+      <div className="container mx-auto break-after-auto">
+        <p className="text-5xl font-bold mb-4">
+          You're not supposed to be here...
+        </p>
+        <Link href="/dashboard">Dashboard</Link>
+      </div>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,18 @@ import React from "react";
 
 const LandingPage: React.FC = () => {
   return (
-    <div>
-      <h1>Leo You</h1>
-      <p>Welcome to the About page! Here, you can learn more about our site.</p>
+    <div className="container mx-auto">
+      <div className="container mx-auto break-after-auto">
+        <p className="text-5xl font-bold mb-4">Leo You</p>
+        <ul className="list-none mt-4 space-y-2 text-lg">
+          <li className="flex items-center">
+            <span className="list-markers mr-2">{">"}</span>
+            <a href="/resume.pdf" download className="underline">
+              Resume
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
