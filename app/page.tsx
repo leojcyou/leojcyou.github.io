fce08d3241fb6a7c9c5327ed99a38d54953ecec6
Leo <108024684+leojcyou@users.noreply.github.com>
2024-12-20 00:26:52 -0500

    padding for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const LandingPage: React.FC = () => {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto px-4">
       <div className="container mx-auto break-after-auto">
         <p className="text-5xl font-bold mb-4">Leo You</p>
         <ul className="list-none mt-4 space-y-2 text-lg">
           <li className="flex items-center">
             <span className="list-markers mr-2">{">"}</span>
-            <a href="/resume.pdf" download className="underline">
+            <a href="/resume.pdf" download="leo_you_resume.pdf" className="underline">
               Resume
             </a>
           </li>
