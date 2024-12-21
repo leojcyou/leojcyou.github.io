46556a9e83884a3b6eeffc2210b92d1424ac691f
Leo <108024684+leojcyou@users.noreply.github.com>
2024-12-21 15:05:31 -0500

    ESLINT

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import Link from "next/link";
+// import Link from "next/link";
 import { ReactTyped } from "react-typed";
 
 const LandingPage: React.FC = () => {
